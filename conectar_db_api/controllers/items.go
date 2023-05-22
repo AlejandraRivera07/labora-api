e9edf941f71afd2fc6557ffe3246ad5d1066b000
Alejandra Rivera <108583326+AlejandraRivera07@users.noreply.github.com>
2023-05-22 15:23:39 -0500

    añadí función update, delet y create

--- conectar_db_api/controllers/items.go
+++ conectar_db_api/controllers/items.go
@@ -69,3 +69,70 @@ func getItemByName(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(item)
 }
+
+func EditItemByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+
+	// Convertir el ID de string a int
+	id, _ := strconv.Atoi(idStr)
+	var itemUpdate models.Item
+	_ = json.NewDecoder(r.Body).Decode(&itemUpdate)
+	log.Printf("Item a actualizar: %+v", itemUpdate)
+	itemUpdated, err := services.UpdateItemByID(id, itemUpdate)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Printf("Item a actualizado: %+v", itemUpdated)
+	jsonData, err := json.Marshal(itemUpdated)
+	if err != nil {
+		log.Printf("Error %v", err)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(jsonData))
+
+}
+
+func DeletItemByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+
+	// Convertir el ID de string a int
+	id, _ := strconv.Atoi(idStr)
+	var itemDelet models.Item
+	_ = json.NewDecoder(r.Body).Decode(&itemDelet)
+	log.Printf("Item a borrar: %+v", itemDelet)
+	itemDeleted, err := services.DeletItemByID(id, itemDelet)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Printf("Item que se borro: %+v", itemDeleted)
+	jsonData, err := json.Marshal(itemDeleted)
+	if err != nil {
+		log.Printf("Error %v", err)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(jsonData))
+}
+
+func CreateItem(w http.ResponseWriter, r *http.Request) {
+	var NewItem models.Item
+	err := json.NewDecoder(r.Body).Decode(&NewItem)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Ocurrió un error al intentar crear el registro"))
+		return
+	}
+	NewItem, err = services.CreateItem(NewItem)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(w).Encode(NewItem)
+}
+
